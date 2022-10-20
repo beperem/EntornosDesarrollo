03fdc2499db0f6cdc067522bc5de070307c6d139
bernardo <bernardopereamenchero@gmail.com>
2022-10-20 17:10:01 +0200

    1

--- src/cuentasBanco/CCuenta.java
+++ src/cuentasBanco/CCuenta.java
@@ -20,7 +20,7 @@ public class CCuenta {
 	private String nombre;
 	private String cuenta;
 	private double saldo;
-	private double tipoInteres;
+	private double tipoIntereses;
 
 	/**
 	 * Constructor vacio
